53e77ac2be85fff42d8b37c73d8522b25a271d09
Henry de Valence <hdevalence@penumbralabs.xyz>
2024-10-02 14:57:52 -0700

    fill in summary totals as placeholder

--- src/accounting.rs
+++ src/accounting.rs
@@ -6,9 +6,12 @@ pub(crate) const NUMOGRAPHICA_FVKS: [&str; 1] = ["NUMOFGRAPHICA_FVK_0001"];
 pub(crate) const PENUMBRA_LABS_BALANCE: u128 = 100_000_000;
 pub(crate) const RADIANT_BALANCE: u128 = 100_000_000;
 pub(crate) const NUMOGRAPHICA_BALANCE: u128 = 100_000_000;
-const INVESTOR_LOCKED_SUPPLY: u128 = 100_000_000;
+
+const TOTAL_LOCKED_GENESIS_SUPPLY: u128 = 38743933_000000;
+const COMMUNITY_POOL_GENESIS_SUPPLY: u128 = 25256067_009667;
 
 #[allow(unused_variables)]
-pub(crate) fn investor_locked_supply(time: DateTime<Utc>) -> u128 {
-    return INVESTOR_LOCKED_SUPPLY;
+pub(crate) fn total_locked_supply(time: DateTime<Utc>) -> u128 {
+    // TODO: replace with dynamic checking of CP balance and unlock schedules
+    return TOTAL_LOCKED_GENESIS_SUPPLY + COMMUNITY_POOL_GENESIS_SUPPLY;
 }
